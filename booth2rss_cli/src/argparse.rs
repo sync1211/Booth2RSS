f2ab567320efca8709098b7e17cfe6714a8fa512
Sync1211 <dpf12110@gmail.com>
2026-08-12 22:04:23 +0200

    Skip first entry when checking commandline arguments The first argument is usually booth2rss_cli itself

--- booth2rss_cli/src/argparse.rs
+++ booth2rss_cli/src/argparse.rs
@@ -23,7 +23,7 @@ pub fn parse_arguments(args: Args) -> Option<CmdParams> {
         convert_target: String::new()
     };
 
-    for arg in args.into_iter() {
+    for arg in args.skip(1).into_iter() {
         match arg.as_str() {
             "--unblur-nsfw" => cmd_params.unblur_nsfw = true,
             "--allow-nsfw" => cmd_params.allow_nsfw = true,
